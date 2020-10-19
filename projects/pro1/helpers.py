139175cf651ae087c7ae082b5c140cbd5a32df5f
chaikum <chaithanyakumar375@gmail.com>
2020-10-19 23:53:08 +0530

    Student commit

--- projects/pro1/helpers.py
+++ projects/pro1/helpers.py
@@ -1,4 +1,19 @@
-print("Good morning1")
-print("Hi bro2")
-print("I have done")
-print('The whole program')
+import logging
+
+
+class Students:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+        if not [self.name, self.marks]:
+            logging.warning("Student name and marks are not defined")
+
+    def result(self):
+        if self.name:
+            logging.warning(self.name)
+        else:
+            logging.warning("The name is not provided")
+
+
+s=Students("Chaithanya", "500")
+s.result()
